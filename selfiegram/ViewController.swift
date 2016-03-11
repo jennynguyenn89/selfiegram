38666f38023e6d88d4b7bb015fee5d5156fd3869
jennynguyenn89 <jennynguyen@jennys-macbook-air.local>
2016-03-10 18:47:12 -0800

    Initial Commit

---
+++ selfiegram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  selfiegram
+//
+//  Created by Jenny Nguyen on 2016-03-10.
+//  Copyright © 2016 Jenny Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
